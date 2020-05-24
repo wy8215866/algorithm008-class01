337a50cf6d50aca49883a6ff42b0784fffd66f7f
lewis <240034372@qq.com>
2020-05-24 21:30:10 +0800

    week04

---
+++ Week_04/leetcode33-搜索旋转排序数组.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int search2(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            // 当[0,mid]有序时,向后规约条件
+            if (nums[0] <= nums[mid] && (target > nums[mid] || target < nums[0])) {
+                lo = mid + 1;
+                // 当[0,mid]发生旋转时，向后规约条件
+            } else if (target > nums[mid] && target < nums[0]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo == hi && nums[lo] == target ? lo : -1;
+    }
+}
+
